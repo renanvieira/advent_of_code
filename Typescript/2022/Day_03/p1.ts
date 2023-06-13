9e05b8ad5eadbfc85976851aff703929c4c68786
Renan Vieira <>
2023-06-13 15:47:33 +0200

    Proper Typescript indentation to 2022 Day 03 code

--- Typescript/2022/Day_03/p1.ts
+++ Typescript/2022/Day_03/p1.ts
@@ -6,35 +6,35 @@ const raw_input = readFileSync(path.join(__dirname, "input.txt"), 'utf8');
 const input = raw_input.split('\n');
 
 
-const itemsPriority : {[key:string]:number} = {}
+const itemsPriority: { [key: string]: number } = {}
 
-for(let i = 0; i<52; i++){
+for (let i = 0; i < 52; i++) {
 
     let letter: string;
 
-    if(i < 26){ // lowercase
-        letter = String.fromCharCode(97+i);
-    } else{ // uppercase
-        letter = String.fromCharCode(65+i-26);
-    } 
+    if (i < 26) { // lowercase
+        letter = String.fromCharCode(97 + i);
+    } else { // uppercase
+        letter = String.fromCharCode(65 + i - 26);
+    }
 
     itemsPriority[letter] = i + 1
 }
 
 let prioritySum = 0;
 
-for(const zack of input){
+for (const zack of input) {
 
-    const half = Math.trunc(zack.length / 2) 
+    const half = Math.trunc(zack.length / 2)
     const firstCompartiment = new Set(zack.slice(0, half))
     const secondCompartiment = new Set(zack.slice(half, zack.length))
-    
-    const intersection = new Set<string>([...firstCompartiment].filter(x => secondCompartiment.has(x)))    
 
-    for(const letter of intersection){
+    const intersection = new Set<string>([...firstCompartiment].filter(x => secondCompartiment.has(x)))
+
+    for (const letter of intersection) {
         prioritySum += itemsPriority[letter]
     }
-    
+
 
 }
 
--- Typescript/2022/Day_03/p2.ts
+++ Typescript/2022/Day_03/p2.ts
@@ -7,43 +7,43 @@ const raw_input = readFileSync(path.join(__dirname, "input.txt"), 'utf8');
 const input = raw_input.split('\n');
 
 // Build the item priority list
-const itemsPriority : {[key:string]:number} = {}
+const itemsPriority: { [key: string]: number } = {}
 
-for(let i = 0; i<52; i++){
+for (let i = 0; i < 52; i++) {
 
     let letter: string;
 
-    if(i < 26){ // lowercase
-        letter = String.fromCharCode(97+i);
-    } else{ // uppercase
-        letter = String.fromCharCode(65+i-26);
-    } 
+    if (i < 26) { // lowercase
+        letter = String.fromCharCode(97 + i);
+    } else { // uppercase
+        letter = String.fromCharCode(65 + i - 26);
+    }
 
     itemsPriority[letter] = i + 1
 }
 
 // Group rugzacks in groups of 3
-let group : Array<Array<Set<string>>> = []
+let group: Array<Array<Set<string>>> = []
 let aux = [new Set(input[0].split(''))]
 
-for(let i=1;i<input.length;i++){    
-     
-    if(i % 3 == 0){
+for (let i = 1; i < input.length; i++) {
+
+    if (i % 3 == 0) {
         group.push(aux)
         aux = []
     }
 
 
     aux.push(new Set(input[i].split('')))
-} 
+}
 
-let prioritySum:number = 0
+let prioritySum: number = 0
 
-for(const g of group){
+for (const g of group) {
 
-    const common = [...g[0]].filter(char => g.every(array=>array.has(char)))
+    const common = [...g[0]].filter(char => g.every(array => array.has(char)))
 
-    for(const badge of common){
+    for (const badge of common) {
         prioritySum += itemsPriority[badge]
     }
 }
